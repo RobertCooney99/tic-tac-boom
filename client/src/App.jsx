4de6f1e61eab7c62fe0a22207b7f7b56dce8fe04
robertcooney <robert.cooney@matillion.com>
2022-07-12 14:30:59 +0100

    feat(TTB-043): Moved header files to common directory

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Game from './Game';
 import Home from './home/Home';
-import Header from './Header';
+import Header from './common/Header';
 
 function App(props) {
   return (
