2dc05ca3119a7a54a3015c2cab8e0730e09c22d8
robertcooney <robert.cooney@matillion.com>
2022-08-04 14:27:03 +0100

    tech(TTB-062): Card.jsx now uses styled-components

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,13 +1,35 @@
-import './card.css';
 import CardIcon from '../CardIcon';
 import CardText from '../CardText';
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+    background-color: #EEE;
+    border-radius: 8px;
+    font-size: 16px;
+    padding: 5px;
+    flex: 1 1 0;
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 250px;
+    margin-left: 15px;
+    margin-right: 15px;
+    transition: background-color 1s ease;
+    text-decoration: none;
+    color: #222;
+
+    &:hover {
+        background-color: #DDD;
+    }
+`;
 
 function Card(props) {
     return(
-        <div className="game-description-card">
+        <CardContainer>
             <CardIcon iconType={props.iconType} />
             <CardText text={props.text} />
-        </div>
+        </CardContainer>
     )
 }
 
