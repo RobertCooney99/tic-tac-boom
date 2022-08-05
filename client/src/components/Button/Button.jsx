8e3fc810e90115a845385caaaf075be8ff81245e
robertcooney <robert.cooney@matillion.com>
2022-08-05 09:01:30 +0100

    tech(TTB-062): Button component using styled-components

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,9 +1,29 @@
-import './button.css';
+import styled from "styled-components";
 import { Link } from 'react-router-dom';
+
+const StyledButton = styled(Link)`
+    background-color: #222;
+    color: #FFF;
+    text-decoration: none;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    padding-left: 30px;
+    padding-right: 30px;
+    border-radius: 8px;
+    transition: background-color 1s ease;
+
+    &:hover {
+        background-color: #444;
+    }
+
+    @media (max-width: 768px) {
+        font-size: 0.8rem;
+    }
+`;
 
 function Button(props) {
     return(
-        <Link className="option-button" to={props.to}>{props.text.toUpperCase()}</Link>
+        <StyledButton to={props.to}>{props.text.toUpperCase()}</StyledButton>
     )
 }
 
