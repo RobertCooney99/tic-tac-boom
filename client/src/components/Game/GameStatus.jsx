e3fddacd5f2911892c1921ca39bcad71c7b6d9ff
robertcooney <robert.cooney@matillion.com>
2022-08-05 13:04:01 +0100

    tech(TTB-062): GameStatus is responsive

--- client/src/components/Game/GameStatus.jsx
+++ client/src/components/Game/GameStatus.jsx
@@ -8,6 +8,15 @@ const StyledStatus = styled.div`
   align-content: center;
   justify-content: center;
   font-size: 20px;
+
+  @media (max-width: 768px) {
+    margin: 15px;
+    font-size: 16px;
+  }
+  @media (max-width: 320px) {
+    margin: 10px;
+    font-size: 14px;
+  }
 `;
 
 function GameStatus({status}) {
